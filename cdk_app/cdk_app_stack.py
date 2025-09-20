7cfbbd85921d4452dd44d6c804fb316b49068032
hopelessRomant <satvolume@gmail.com>
2025-09-20 06:22:02 +0000

    cleanup

--- cdk_app/cdk_app_stack.py
+++ cdk_app/cdk_app_stack.py
@@ -1,19 +1,9 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+  Stack,
 )
 from constructs import Construct
 
-class CdkAppStack(Stack):
+class HelloCdkStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkAppQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+  def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
