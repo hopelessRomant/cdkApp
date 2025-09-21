dc8852453ae5a8165a9021cdfe7e1944644ce6b1
hopelessRomant <satvolume@gmail.com>
2025-09-21 04:24:20 +0000

    aws env config

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from cdk_app.cdk_app_stack import CdkAppStack
+from cdk_app.cdk_app_stack import HelloCdkStack
 
 
 app = cdk.App()
-CdkAppStack(app, "CdkAppStack",
+HelloCdkStack(app, "HelloCdkStack",
         env = cdk.Environment(
             account=os.getenv("ACCOUNTID"), 
             region=os.getenv("REGION")
