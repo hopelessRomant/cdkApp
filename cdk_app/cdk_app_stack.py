b3e3baafad84eadbfab8ca90c0bcd53c60dfb897
hopelessRomant <satvolume@gmail.com>
2025-09-21 12:46:27 +0000

    lambda function

--- cdk_app/cdk_app_stack.py
+++ cdk_app/cdk_app_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
   Stack,
+  aws_lambda as _lambda,
 )
 from constructs import Construct
 
@@ -7,3 +8,20 @@ class HelloCdkStack(Stack):
 
   def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
+
+    my_function = _lambda.Function(
+      self, "MyFirstFunction",
+      runtime= _lambda.Runtime.NODEJS_20_X,
+      handler="index.handler",
+      code= _lambda.Code.from_inline(
+        """
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World'),
+          };
+        };
+        """
+      ),
+
+    )
